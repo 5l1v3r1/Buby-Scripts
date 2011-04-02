8bf52ccfb897b4f5029a1398c8655994397e3d2d
cktricky <cktricky@192.168.1.106>
2011-04-02 04:46:37 -0400

    added another method to link_extractor

--- link_extractor.rb
+++ link_extractor.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env jruby
   
-  
+
+=begin  
   #
   # Basic method to demonstrate extracting links from a site map
   # ...this example uses the site http://www.cnn.com as the URL we'd
@@ -16,4 +17,29 @@
       end
     return super(*param)
   end
- 
+=end
+
+
+ #
+ # This method checks, upon every request, if the request's URL is
+ # ...in scope. If it is, burp will write the links to a links.txt file.
+ #
+ def $burp.evt_proxy_message(*param)
+   msg_ref, is_req, rhost, rport, is_https, http_meth, url, resourceType, status, req_content_type, message, action = param
+     if is_https == true
+       prefix = "https://"
+     else
+       prefix = "http://"
+     end
+   rurl = "#{prefix}#{rhost}"  
+   if (rurl) and $burp.isInScope(rurl)
+     $burp.get_site_map(rurl).each do |item|
+       file = ('links.txt')
+         if item.respond_to?('url')
+           File.open(file, "a") {|f| f.write("#{item.url}\n")}
+         end
+     end
+   end
+  
+   return super(*param)
+ end
