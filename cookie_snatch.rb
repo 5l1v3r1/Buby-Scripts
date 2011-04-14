55a25f2ce3df52e8099b92f3eff42082dc860ce8
cktricky <cktricky@192.168.1.109>
2011-04-14 00:22:56 -0400

    added comments

--- cookie_snatch.rb
+++ cookie_snatch.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+  
+  # Method reviews the request, then the cookie which has been set
+  # ...is logged to cookiez.txt in the directory your running the
+  # ...script from.
+  
   def $burp.evt_proxy_message(*param)
   msg_ref, is_req, rhost, rport, is_https, http_meth, url, resourceType, status, req_content_type, message, action = param
    file = ('cookiez.txt')
@@ -9,14 +14,16 @@
    else
      prefix = 'http'     
    end
+   # Uncomment commented items below item to check if the URL you are requesting
+   # ...is in scope. If so, only THEN will the cookie be logged.
    #rurl = "#{prefix}://#{rhost}:#{rport}"
    if is_req == false
     spmsg = message.split("\n\n")
     short_msg = "#{spmsg}"
-    mitem = short_msg.match(/^Set-Cookie:.+$/)
+    mitem = short_msg.match(/^Set-Cookie:.+$/) 
     #  if $burp.in_scope?(rurl)  
         if !mitem.nil?
-          File.open(file, "a") {|f| f.write("#{mitem}")}
+          File.open(file, "a") {|f| f.write("#{mitem}")} #Change the "a" to "w" if you'd like to overwrite with each set-cookie
         end
      # end
    end   
