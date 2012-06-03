ae93f04e6d0696094e226d1ef637eebff09c9eb0
cktricky <enduser@Livings-MacBook-Pro-148.local>
2012-06-03 12:50:00 -0400

    tinkering around

---
+++ gzip_decompression/compression.rb
@@ -0,0 +1,163 @@
+#!/usr/bin/env ruby
+
+require 'java'
+require 'zlib'
+require 'stringio'
+import java.awt.Dimension
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.JTabbedPane
+import javax.swing.GroupLayout
+import javax.swing.JButton
+import javax.swing.SwingConstants
+import java.awt.event.WindowEvent
+import javax.swing.JOptionPane
+import javax.swing.JTextArea
+import javax.swing.JScrollPane
+
+class GzipFuPanel < JPanel
+  
+  def initialize(frame)
+    @frame = frame
+    super()
+    initUI
+  end
+  
+  def initUI
+    
+    @close_b = JButton.new("Exit")
+    @close_b.add_action_listener do |e|
+      @frame.close_it     
+    end
+    
+    @send_b = JButton.new("Forward")
+    @ta1 = JTextArea.new
+    @sp1 = JScrollPane.new(@ta1)
+   
+    #
+    # GROUP LAYOUT OPTIONS
+    #
+    
+    layout = GroupLayout.new self
+    # Add Group Layout to the frame
+    self.setLayout layout
+    # Create sensible gaps in components (like buttons)
+    layout.setAutoCreateGaps true
+    layout.setAutoCreateContainerGaps true
+  
+    sh1 = layout.createSequentialGroup
+    sv1 = layout.createSequentialGroup
+    sv2 = layout.createSequentialGroup
+    sv3 = layout.createSequentialGroup
+
+    p1 = layout.createParallelGroup
+    p2 = layout.createParallelGroup
+    
+    layout.setHorizontalGroup sh1
+    layout.setVerticalGroup sv3
+    
+    sv1.addComponent(@sp1)
+    sv2.addComponent(@close_b)
+    sv2.addComponent(@send_b)
+    sv3.addGroup(sv1)
+    sv3.addGroup(sv2)
+    
+    
+    p2.addComponent(@close_b)
+    p2.addComponent(@send_b)
+    sh1.addComponent(@sp1)
+    sh1.addGroup(p2)
+    
+    layout.linkSize SwingConstants::HORIZONTAL, 
+        @close_b, @send_b
+    
+  end
+  
+
+end 
+
+class GzipFuTabbedPane < JTabbedPane
+  
+  def initialize(frame)
+    super(JTabbedPane::TOP, JTabbedPane::SCROLL_TAB_LAYOUT)
+    @gfp = GzipFuPanel.new(frame)
+    add("Test1", @gfp)
+  end 
+  
+end 
+
+class GzipFuFrame < JFrame
+  
+  def initialize
+    super("GZip-F.U.")
+    init
+  end 
+  
+  def init
+    
+    self.addWindowListener do |e|
+      if e.kind_of?(WindowEvent)
+       ps = e.paramString.split(',')[0] == "WINDOW_CLOSING" ? true : false
+       if (ps)
+         close_it
+       end        
+      end
+     end
+     
+    gftp = GzipFuTabbedPane.new(self)
+    self.add gftp
+    
+    # Set the overall side of the frame
+    self.setJMenuBar menuBar
+    self.setPreferredSize Dimension.new(1300, 900)
+    self.pack
+  
+    self.setDefaultCloseOperation JFrame::DO_NOTHING_ON_CLOSE
+    self.setLocationRelativeTo nil
+    self.setVisible true 
+  end
+  
+  def close_it
+    jo = JOptionPane.showConfirmDialog(nil, 
+           "Exit confirmation will also close Burp!", 
+           "Confirmation", 
+           javax.swing.JOptionPane::YES_NO_OPTION, 
+           javax.swing.JOptionPane::QUESTION_MESSAGE)
+    if jo == JOptionPane::YES_OPTION
+      java.lang.System.exit 0 
+    end
+  end
+  
+end
+
+
+def $burp.evt_proxy_message(*param)
+    msg_ref, is_req, rhost, rport, is_https, http_meth, url, resourceType, status, req_content_type, message, action = param
+ @str = ''
+ @e = nil  
+  if not is_req 
+    msg = message.split(/\r\n\r\n/)
+    body = msg[1]
+    begin
+      if not body.nil?
+        gz = Zlib::GzipReader.new(StringIO.new(body))
+        msg[1] = gz.read
+        gz.close
+        @str << "#{msg[0]}".gsub!(/Content-Encoding: gzip/, "")
+        @str << "\r\n\r\n#{msg[1]}"
+      end     
+    rescue Exception => @e
+    end 
+      if @e
+        param = msg_ref, is_req, rhost, rport, is_https, http_meth, url, resourceType, status, req_content_type, message, action
+       return super(*param)
+      else
+         param = msg_ref, is_req, rhost, rport, is_https, http_meth, url, resourceType, status, req_content_type, @str, action
+         return super(*param)
+      end 
+   else
+     return super(*param)
+  end   
+end
+
+GzipFuFrame.new
